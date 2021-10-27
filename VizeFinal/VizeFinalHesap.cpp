90bb2033365977085fa209eb556decdada25bdfd
Eren <ernsngrsocial@gmail.com>
2021-10-27 13:43:14 +0300

    update

--- VizeFinal/VizeFinalHesap.cpp
+++ VizeFinal/VizeFinalHesap.cpp
@@ -7,36 +7,30 @@
 
 int main(){
     
-    int sinif[5][3]; 
-    float ortalama[5];
+int sinif[5][2]; 
+float ortalama[5];
 
-    printf("\tLutfen 0-100 Arasi Not Giriniz\n");
-        for(int i = 0 ; i < 5; i += 1){
-        printf("%d. ogrencinin ;\n",i+1);
-        printf("Vize 1 Notu : ");
-        scanf("%d",&sinif[i][0]); // sütunda 0. index vize 1 e 
-        printf("Vize 2 Notu :");
-        scanf("%d",&sinif[i][1]); // sütunda 1. index vize 2 ye 
-        printf("Final Notu :");
-        scanf("%d",&sinif[i][2]); // sütunda 2. index final e denk gelir    
+printf("\tLutfen 0-100 Arasi Not Giriniz\n");
+for(int i = 0 ; i < 5; i += 1){
+    printf("%d. ogrencinin ;\n",i+1);
+    printf("Vize Notu : ");
+    scanf("%d",&sinif[i][0]);
+    while(sinif[i][0] < 0 || sinif[i][0] > 100){
+        printf("Lutfen %d. Ogrencinin Vize Notunu Belirtilen Aralikta Giriniz: ",i+1);
+        scanf("%d",&sinif[i][0]);
+    }
+    printf("Final Notu :");
+    scanf("%d",&sinif[i][1]); 
+    while(sinif[i][1] < 0 || sinif[i][1] > 100){
+        printf("Lutfen %d. Ogrencinin Final Notunu Belirtilen Aralikta Giriniz: ",i+1);
+        scanf("%d",&sinif[i][1]);
+}
 
-        }
-    
-        for(int i = 0 ; i < 5 ; i += 1){
-             if(sinif[i][0] > 0 & sinif[i][0] <= 100 & sinif[i][1] > 0 & sinif[i][1] <= 100 & sinif[i][2] > 0 & sinif[i][2] <= 100){
-                 for(int i = 0 ; i < 5 ; i++){
-                    ortalama[i] = (sinif[i][0] * 0.25) + (sinif[i][1] * 0.25) + (sinif[i][2] * 0.50);
-                }
-                 printf("%d. ogrencinin ortalaması : %.02f\n",i+1,ortalama[i]);
-             }
-             else{
-                 printf("%d. Ogrencinin Notu Belirtilen Aralikta Degildir\n",i+1);
-             }
-        }
+}
+for (int i = 0; i < 5; i++){
+    ortalama[i] = (sinif[i][0] * 0.4) + (sinif[i][1] * 0.6);
+    printf("%d. Ogrencinin Donem Ortalaması : %.1f\n",i+1,ortalama[i]);
+}
 
-    
-    
-    
-    
     return 0;
 }
