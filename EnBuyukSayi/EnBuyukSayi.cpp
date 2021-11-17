13414f73590b2dd45be21f7976ad1265ed51ee75
Eren <ernsngrsocial@gmail.com>
2021-11-17 16:35:09 +0300

    girilen en buyukk sayının indis numarasını gösteren program

--- EnBuyukSayi/EnBuyukSayi.cpp
+++ EnBuyukSayi/EnBuyukSayi.cpp
@@ -1,18 +1,25 @@
 #include <stdio.h>
 // girilen 10 adet sayının en büyüğünün kaçıncı sayı olduğunu bulan algoritmayı yazınız
 int main(){
-    int sayi[10], enBuyuk = 0, i = 0, sira;
+    int sayi[10], i = 0;
+    
     while(i < 10){
-        printf("Lutfen %d. Sayiyi Giriniz: ",i+1);
+        printf("Lutfen %d. Sayiyi Giriniz :",i+1);
         scanf("%d",&sayi[i]);
-        if (sayi[i] > enBuyuk)
-        {
-            enBuyuk = sayi[i];
-            sira = i+1;
+        i++;
+    }
+    i = 0;
+    int EnBuyuk = sayi[i], sayac = 0;
+    while(i < 10){
+        if(sayi[i] > EnBuyuk){
+            EnBuyuk = sayi[i];
+            sayac = i;
         }
         i++;
     }
-    printf("Girilen En Buyuk ve %d. Sayi %d\n",sira,enBuyuk);
+    printf("En Buyuk sayi :%d\n",EnBuyuk);
+    printf("İndis Numarası %d",sayac);
+    
     
     return 0;
     
